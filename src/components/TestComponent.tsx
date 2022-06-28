4199a6915cc63714c01b27ef1c6e36d55d993f47
Lokhtin Denis <denis.lokhtin@gmail.com>
2022-06-28 23:23:27 +0600

    initial commit

---
+++ src/components/TestComponent.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Box} from "@mui/material";
+
+const TestComponent = () => {
+  return (
+    <Box className='test'>
+      Test message
+    </Box>
+  );
+};
+
+export default TestComponent;
---
+++ src/containers/App.tsx
@@ -0,0 +1,13 @@
+import TestComponent from "../components/TestComponent";
+
+
+
+function App() {
+  return (
+    <div className="app">
+      <TestComponent />
+    </div>
+  )
+}
+
+export default App;
